a2bfa54d6a23e1b1759a5fc12e31e0ff4fae1c13
ilianvo <119301418+ilianvo@users.noreply.github.com>
2023-01-31 04:23:20 +0200

    Delete script.sh

--- modules/codebuild/script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-aws codebuild import-source-credentials \
-  --token "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" \
-  --server-type GITHUB \
-  --auth-type PERSONAL_ACCESS_TOKEN \
-  --region eu-west-3
