598cfdfd4819f9853cf5a9a44014786573087eac
ilianvo <119301418+ilianvo@users.noreply.github.com>
2024-07-26 20:32:37 +0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ import random
 app = Flask(__name__)
 # image of jenkins
 images = [
-  "https://www.jenkins.io/images/logos/jenkins-x/jenkins-x.png",
+  "https://www.jenkins.io/images/logos/beekeeper/beekeeper.png",
 ]
 @app.route('/')
 def index():
